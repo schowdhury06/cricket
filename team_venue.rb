5a2b4072d986e2b793d3e9ab5cb13e6f95f18561
schowdhury06 <shyd.chowdhury444.gmail.comgit config --global user.name shydgit remote add origin https://github.com/schowdhury06/new_git.gitgit push -u origin mastergit config --global shyd.chowdhury444@gmail.com>
2016-03-05 12:48:40 -0600

    added new files

---
+++ team_venue.rb
@@ -0,0 +1,25 @@
+class Team
+  attr_accessor :bowler, :batsman, :feilder, :coach, :manager
+  
+  def initialize (blower, batsman, feilder, coach, manager)
+  @bowler = bowler
+  @batsman = batsman
+  @feilder = feilder
+  @ coach = coach
+  @ manager = manager
+  end
+  end
+  class venue
+  attr_accessor :location, :seats, :gates, :tickets
+  
+  def initialize(location,seats,gates,tickets)
+  @location = location
+  @seats	= seats
+  @gates = gates
+  @tickets = tickets
+  end
+ 
+  end
+  
+  
+ 
